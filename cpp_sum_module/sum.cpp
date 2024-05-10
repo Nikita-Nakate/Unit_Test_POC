9fcef4efc25c369caba3ef6853628055eb185619
Nikita-Nakate <nikita.nakate@agiliad.com>
2024-05-10 11:20:25 +0530

    Update sum.cpp
    Removed main block from sum.cpp

--- cpp_sum_module/sum.cpp
+++ cpp_sum_module/sum.cpp
@@ -5,13 +5,3 @@
 int calculateSum(int a, int b) {
     return a + b;
 }
-
-int main() {
-    int num1 = 10;
-    int num2 = 20;
-    int result = calculateSum(num1, num2);
-    
-    std::cout << "Sum: " << result << std::endl;
-    
-    return 0;
-}
